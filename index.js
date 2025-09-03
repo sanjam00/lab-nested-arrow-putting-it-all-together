725c0e19323c2b5d7bfa4aee19353573db7254df
Sanaya James <sanaya.j.james@gmail.com>
2025-09-03 14:42:07 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-
-
-
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+
+function createLoginTracker(userInfo) {
+
+  let attemptCount = 0;
+
+  const loginAttempt = (passwordAttempt) => {
+    attemptCount++;
+
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else if (passwordAttempt !== userInfo.password) {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+
+  }
+
+  return loginAttempt;
+}
+
+const tracker = createLoginTracker({
+  username: "james123",
+  password: "password321"
+})
+
+
+console.log(tracker("wrongpass"));
+console.log(tracker("wrongpass"));
+console.log(tracker("wrongpass"));
+console.log(tracker("password321"));
